fd3f0dc61f2c45ea7825c7cf1ae5f7f559b32b22
Marinarty <tsiganosha@yandex.ru>
2019-09-20 11:53:59 +0200

    правки линтера

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var CLOUD_WIDTH = 420; //высота облака
-var CLOUD_HEIGHT = 270; //ширина облака
-var CLOUD_X = 100; //координата верхнего края облака по Х
-var CLOUD_Y = 10;  //координата верхнего края облака по У
-var GAP = 30; //паддинг облака
-var GAP_SHADOW = 10; //отступ тени облака
-var COLOMN_GAP = 50; //расстояние между колонками
-var COLOMN_WIDTH = 40; //ширина колонки
-var TEXT_HEIGHT = 10; //высота текста
-var barWidth = 100; //номинальная высота колонки
+var CLOUD_WIDTH = 420; // высота облака
+var CLOUD_HEIGHT = 270; // ширина облака
+var CLOUD_X = 100; // координата верхнего края облака по Х
+var CLOUD_Y = 10; // координата верхнего края облака по У
+var GAP = 30; // паддинг облака
+var GAP_SHADOW = 10; // отступ тени облака
+var COLOMN_GAP = 50; // расстояние между колонками
+var COLOMN_WIDTH = 40; // ширина колонки
+var TEXT_HEIGHT = 10; // высота текста
+var barWidth = 100; // номинальная высота колонки
 
 
 // Отрисовывает облако со статистикой и тень под ним
@@ -17,7 +17,7 @@ var barWidth = 100; //номинальная высота колонки
 // @param {number} x координата Х начала отрисовки прямоугольгика в пикселях
 // @param {number} y координата Y начала отрисовки прямоугольгика в пикселях
 // @param {} color
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
@@ -25,7 +25,7 @@ var renderCloud = function(ctx, x, y, color) {
 // Находит максимальное время в массиве times
 // @param {} arr Массив
 // @return {number} maxElement Максимальнео время прохождения игры
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -41,7 +41,7 @@ var getMaxElement = function(arr) {
 // @param {} ctx контекст канваса
 // @param {string} players Имена игроков
 // @param {number} times Время прохождение игры
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP_SHADOW, CLOUD_Y + GAP_SHADOW, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
